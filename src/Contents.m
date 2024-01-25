82db7b7fd382860ff76e24700e5457935f7013d8
apambrose <austin.p.ambrose@gmail.com>
2024-01-25 01:09:31 -0800

    v0.2.0.12

--- src/Contents.m
+++ src/Contents.m
@@ -1,5 +1,5 @@
 % MATVISA - A MATLAB wrapper for the NI-VISA
-% Version 0.2.0.11 25-Jan-2024
+% Version 0.2.0.12 25-Jan-2024
 %
 % My Classes
 %   matvisa            - Find/control test and measurement instruments
